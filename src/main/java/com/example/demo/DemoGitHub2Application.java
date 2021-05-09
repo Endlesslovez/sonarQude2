8a660a8e47d1911d1bb3b37115773c5b2639e491
minhminh <nguyenxuanminh10@gmail.com>
2021-05-09 21:31:50 +0700

    áds

--- src/main/java/com/example/demo/DemoGitHub2Application.java
+++ src/main/java/com/example/demo/DemoGitHub2Application.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoGitHub2Application {
@@ -10,4 +11,87 @@ public class DemoGitHub2Application {
 		SpringApplication.run(DemoGitHub2Application.class, args);
 	}
 
+	public boolean mainsd(String name) {
+		return name.equals(name);
+	}
+	
+	public boolean  mainsddsd(String name) {
+		return name.equals(name);
+	}
+
+	public boolean  mainsddsd1(String name) {
+		return name.equals(name);
+	}
+	@Bean
+	public void demo1() {
+		mainsd("minh");
+		mainsddsd1("caominh");
+	}
+	
+	@Bean
+	public void Dmo12() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
+	
+	@Bean
+	public void Dmos12() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
+	
+	
+	@Bean
+	public void Dmos12ssds() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
+	
+	
+	@Bean
+	public void Dmoss12() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
+	
+
+	@Bean
+	public void Dmoss12d() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
+
+	@Bean
+	public void Dmosdsss12d() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
+	
+
+
+	@Bean
+	public void Dmosdssssd12d() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
+
+	@Bean
+	public void Dmosddssssd12d() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
+	
+
+	@Bean
+	public void Dmosddsssdsd12d() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
+	
+
+	@Bean
+	public void Dmosddsssdsdsd12d() {
+		mainsddsd1("caominh");
+		mainsddsd("dsdsds");
+	}
 }
